158f5b84aeac46ce72cfda451abe864333c5e74c
nguyen dinh khanh <khanhnd@ftech.ai>
2023-07-26 14:52:30 +0700

    rm gitlab

--- deploy_it.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-yarn build-it
-scp -r dist arrow@192.168.1.4:/home/arrow/projects/jira_qcd_test/jira_qcd_client
--- deploy_prod.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-yarn build-prod
-scp -r dist/* honglm1011@34.126.157.89:/var/www/qcd/
